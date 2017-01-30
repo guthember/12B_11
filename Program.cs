f6f40c8769e59ea3335719d2a73e0c2f25399b5a
Erik Gúth <guthember@gmail.com>
2017-01-30 10:17:58 +0100

    Megoldva

--- Program.cs
+++ Program.cs
@@ -22,11 +22,36 @@ namespace Statisztika
             
             Be.Close();
             file.Close();
+            // leghosszabb adat a fájlban? 
+            // kiiratni a méretét
+            // feltételezés
+            int max = adatok[0].Length;
+            int min = adatok[0].Length;
 
+            // Adatok kiírása
             foreach (string sz in adatok)
             {
-                Console.WriteLine(sz);
+                Console.WriteLine("{0}({1})",sz,sz.Length);
             }
+            
+            // Vizsgálat 2. elemtől a végéig
+            for (int i = 1; i < adatok.Count; i++)
+            {
+                // Van-e nagyobb érték?
+                if (adatok[i].Length > max)
+                {
+                    max = adatok[i].Length;
+                }
+
+                // Van-e kissebb érték?
+                if (adatok[i].Length < min)
+                {
+                    min = adatok[i].Length;
+                }
+            }
+
+            Console.WriteLine("A leghosszabb adat: {0} db karakter.", max);
+            Console.WriteLine("A legrövidebb adat: {0} db karakter.", min);
 
             Console.ReadLine();
         }
