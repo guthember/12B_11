06b0f0e30aed67a0e14a8e2f0c93e330f9bf675d
Erik Gúth <guthember@gmail.com>
2017-01-30 09:21:46 +0100

    Kezdés

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Statisztika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> adatok = new List<string>();
+
+            FileStream file = new FileStream(@"C:\Users\guthe\documents\visual studio 2010\Projects\Statisztika\adat.txt", FileMode.Open);
+            StreamReader Be = new StreamReader(file);
+
+            while (!Be.EndOfStream)
+            {
+                adatok.Add(Be.ReadLine());
+            }
+            
+            Be.Close();
+            file.Close();
+
+            foreach (string sz in adatok)
+            {
+                Console.WriteLine(sz);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
